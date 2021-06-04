500107d05586c13aa674397a9ed51290ed1f2b05
LorranPdS <lorransantospereira@yahoo.com.br>
2021-06-04 20:40:20 -0300

    primeira compilacao typescript

---
+++ 01compilar.js
@@ -0,0 +1,14 @@
+/** Quanto a essa extensão '.ts', ele irá basicamente abraçar todo o conteúdo
+ * que foi visto desde o ECMAScript 6 (que é o JavaScript) e contemplar
+ * alguns comandos extras do typescript, mas antes de desbravarmos esses
+ * conteúdos extras, vamos entender como compilamos um typescript, porque
+ * um arquivo '.ts' não é executado pelo browser (o browser não consegue
+ * entender a extensão '.ts', mesmo você escrevendo esse código em JavaScript);
+ *
+ * Sendo assim, o que faremos é um comando bem simples.
+ */
+"olá mundo";
+/** Esse tsc abaixo vem de typescript. Ao executar o comando, fique observando
+ * o estado em que vai ficar o arquivo na coluna ao lado
+ */
+alert("Olá mundo!");
