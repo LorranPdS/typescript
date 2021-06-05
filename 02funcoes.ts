814d555e8e51f3b0240d001765c0b233905eeeb7
LorranPdS <lorransantospereira@yahoo.com.br>
2021-06-04 21:42:16 -0300

    Criando funcoes com typescript

---
+++ 02funcoes.ts
@@ -0,0 +1,25 @@
+/** Criamos mais um novo arquivo, mas agora para criarmos funções. Imagine
+ * que você queira criar várias ações para o seu site, então para isso
+ * nós podemos usar algumas funções que temos no JavaScript 6.
+ * Vamos então criar uma function abaixo.
+ */
+
+/*
+function mensagem(){
+
+}
+*/
+
+// Função de mensagem
+const mensagem = () => {
+    window.alert("Olá mundo!")
+}
+
+/* Então veja o que foi feito:
+mensagem - nome da minha função
+() - é uma função sem parâmetro
+=> função arrow
+{entre as chaves} é onde teremos o nosso retorno
+
+Feito isso, vamos compilar e ver como fica isso no JavaScript
+*/
